fc7dc1105e611bc0e098ea1f6d6e66ce3c69adf7
codersparks <codersparks@codersparks.uk>
2020-11-07 11:34:52 +0000

    Initial commit

---
+++ src/main/java/uk/codersparks/homeassistant/dennonservices/DennonServicesApplication.java
@@ -0,0 +1,13 @@
+package uk.codersparks.homeassistant.dennonservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DennonServicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DennonServicesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uk/codersparks/homeassistant/dennonservices/DennonServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.codersparks.homeassistant.dennonservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DennonServicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
